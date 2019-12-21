df3c767082e55000f3356c29db205193e0dca670
UlyanaLY <oulyana@gmail.com>
2019-12-21 20:10:56 +0300

    Добавила функцию расчёта расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/at/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/at/sandbox/MyFirstProgram.java
@@ -2,6 +2,13 @@ package ru.stqa.at.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hi!");
+		Point p1 = new Point(5.0, 5.0);
+		Point p2 = new Point(5.0, 5.0);
+		System.out.println("Расстояние между точкой p1 и p2 составляет" + " " + distance(p1, p2));
+	}
+
+	public static double distance(Point p1, Point p2) {
+		double distance = Math.sqrt(Math.pow((p2.x - p1.x), 2.0) + Math.pow((p2.y - p1.y), 2.0));
+		return distance;
 	}
 }
