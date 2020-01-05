a5c2d04f4c2d29e9ebf2b67666af2e4127ceaf0b
UlyanaLY <oulyana@gmail.com>
2020-01-05 11:14:43 +0300

    Оптимизирован переход между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/at/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/at/addressbook/appmanager/NavigationHelper.java
@@ -3,21 +3,35 @@ package ru.stqa.at.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
 	public NavigationHelper(WebDriver wd) {
 		super(wd);
 	}
 
 	public void goToHomePage() {
+		if (isElementPresent(By.id("maintainable"))) {
+			return;
+		}
 		click(By.linkText("home"));
 	}
 
 	public void goToGroupPage() {
-		click(By.linkText("groups"));
+		if (isElementPresent(By.tagName("h1"))
+						&& wd.findElement(By.tagName("h1")).getText().equals("Groups")
+						&& isElementPresent(By.name("new"))) {
+			return;
+		} else {
+			click(By.linkText("groups"));
+		}
 	}
 
 	public void goToCreateContactPage() {
-		click(By.linkText("add new"));
+		if (isElementPresent(By.tagName("h1"))
+						&& wd.findElement(By.tagName("h1")).getText().equals("isElementPresent(By.tagName(\"h1\"))")) {
+			return;
+		} else {
+			click(By.linkText("add new"));
+		}
 	}
 }
