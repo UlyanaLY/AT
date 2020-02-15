ada975a5041d26c513521a554d92afc455a65d86
UlyanaLY <oulyana@gmail.com>
2020-02-15 18:35:15 +0300

    Описание тестового стенда вынесено в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/at/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/at/addressbook/appmanager/ApplicationManager.java
@@ -4,10 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.BrowserType;
 
-public class ApplicationManager {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
+public class ApplicationManager {
+  private final Properties properties;
 	protected WebDriver wd;
 
 	private SessionHelper sessionHelper;
@@ -18,28 +25,31 @@ public class ApplicationManager {
 
 	public ApplicationManager(String browser) {
 		this.browser = browser;
+		properties = new Properties();
 	}
 
-	public void init() {
+	public void init() throws IOException {
+		String target = System.getProperty("target", "local");
+		properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
 		if (browser.equals(BrowserType.CHROME)) {
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Documents\\GitHub\\drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", properties.getProperty("web.chromeDriverPath"));
 			wd = new ChromeDriver();
 		} else if (browser.equals(BrowserType.IE)) {
-			System.setProperty("webdriver.ie.driver", "C:\\Users\\PC\\Documents\\GitHub\\drivers\\IEDriverServer.exe");
+			System.setProperty("webdriver.ie.driver", properties.getProperty("web.fireFoxDriverPath"));
 			wd = new InternetExplorerDriver();
 			wd.manage().window().maximize();
 		} else if (browser.equals(BrowserType.FIREFOX)) {
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\PC\\Documents\\GitHub\\drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", properties.getProperty("web.ieDriverPath"));
 			wd = new FirefoxDriver();
 		}
 
-		wd.get("http://localhost/addressbook/");
+		wd.get(properties.getProperty("web.baseUrl"));
 		groupHelper = new GroupHelper(wd);
 		navigationHelper = new NavigationHelper(wd);
 		sessionHelper = new SessionHelper(wd);
 		contactHelper = new ContactHelper(wd);
-		sessionHelper.login("admin", "secret");
+		sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
 	}
 
 	public void stop() {
