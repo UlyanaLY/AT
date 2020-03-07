ff6652615ac729a6a9b3095950d2e41d4f1ee30e
UlyanaLY <oulyana@gmail.com>
2020-03-07 09:06:21 +0300

    Попытка изменить кодировку

--- mantis-tests/src/test/java/ru/stqa/at/mantis/appmanager/UsersHelper.java
+++ mantis-tests/src/test/java/ru/stqa/at/mantis/appmanager/UsersHelper.java
@@ -11,14 +11,14 @@ public class UsersHelper extends HelperBase {
 	public void start(String username, String password) {
 		wd.get(app.getProperty("web.baseUrl") + "login_page.php");
 		type(By.name("username"), username);
-		click(By.cssSelector("input[value='Войти']"));
+		click(By.cssSelector("input[value='Р’РѕР№С‚Рё']"));
 		type(By.name("password"), password);
-		click(By.cssSelector("input[value='Войти']"));
+		click(By.cssSelector("input[value='Р’РѕР№С‚Рё']"));
 	}
 
 	public void resetPassword(String userId) {
 		wd.get(app.getProperty("web.baseUrl") + "manage_user_edit_page.php?user_id=" + userId);
-		click(By.cssSelector("input[value='Сбросить пароль']"));
+		click(By.cssSelector("input[value='РЎР±СЂРѕСЃРёС‚СЊ РїР°СЂРѕР»СЊ']"));
 	}
 
 	public void changeAccount(String confirmationLink, String username) {
@@ -26,7 +26,7 @@ public class UsersHelper extends HelperBase {
 		type(By.name("realname"), username);
 		type(By.name("password"), "newpwd");
 		type(By.name("password_confirm"), "newpwd");
-		click(By.xpath("//span[contains(text(), 'Изменить')]"));
+		click(By.xpath("//span[contains(text(), 'РР·РјРµРЅРёС‚СЊ')]"));
 	}
 
 
