750f123945c10a981656c5674c427a04b34f2368
UlyanaLY <oulyana@gmail.com>
2020-03-11 00:50:53 +0300

    Интеграция тестов с багтрекером Mantis(19 задание)

--- mantis-tests/src/test/java/ru/stqa/at/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/at/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
 	private FtpHelper ftp;
 	private MailHelper mailHelper;
 	private DbHelper dbHelper;
+	private SoapHelper soapHelper;
 
 	public ApplicationManager(String browser) {
 		this.browser = browser;
@@ -96,5 +97,12 @@ public class ApplicationManager {
 			dbHelper = new DbHelper(this);
 		}
 		return dbHelper;
+	}
+
+	public SoapHelper soap() {
+		if (soapHelper == null){
+			soapHelper = new SoapHelper(this);
+		}
+		return soapHelper;
 	}
 }
--- mantis-tests/src/test/java/ru/stqa/at/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/at/mantis/tests/TestBase.java
@@ -1,13 +1,21 @@
 package ru.stqa.at.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import biz.futureware.mantis.rpc.soap.client.ObjectRef;
 import org.openqa.selenium.remote.BrowserType;
 
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.at.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -19,6 +27,21 @@ public class TestBase {
 
 		app.init();
 		app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.back");
+	}
+
+	public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+		MantisConnectPortType mc = app.soap().getMantisConnect();
+		IssueData issue = mc.mc_issue_get("administrator", "root", BigInteger.valueOf(issueId));
+
+		ObjectRef status = issue.getStatus();
+
+		return !status.getName().equals("resolved");
+	}
+
+	public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+		if (isIssueOpen(issueId)) {
+			throw new SkipException("Ignored because of issue " + issueId);
+		}
 	}
 
 	@AfterSuite(alwaysRun = true)
