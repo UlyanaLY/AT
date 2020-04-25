32568f628908e36643b12b9a888ed4c8352c1016
UlyanaLY <oulyana@gmail.com>
2020-04-25 12:19:46 +0300

    Доработка для 19 задания

--- rest-sample/src/test/java/ru/stqa/at/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/at/rest/RestTests.java
@@ -15,7 +15,7 @@ public class RestTests extends TestBase {
     @Test
     public void testCreateIssue() throws IOException {
         Set<Issue> oldIssues = getIssues();
-        Issue newIssue = new Issue().withSubject("Issue 778").withDescription("Description778").withState("0").withState_name("Opened");
+        Issue newIssue = new Issue().withSubject("Issue 779").withDescription("Description779").withState("0").withState_name("Open");
         //Спросить, почему нет возможности создать Resolved
         int issueId = createIssue(newIssue);
         Set<Issue> newIssues = getIssues();
